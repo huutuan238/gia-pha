5b375c835a552d1fdc53a819885ffcb6a9769851
huutuan238 <huutuan238@gmail.com>
2026-07-18 23:08:50 +0900

    feat: add table family

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,12 +2,17 @@ from .extensions import db
 
 
 class Person(db.Model):
-
     __tablename__ = "persons"
 
     id = db.Column(
         db.String(36),
         primary_key=True
+    )
+
+    family_id = db.Column(
+        db.String(36),
+        db.ForeignKey("families.id"),
+        nullable=True
     )
 
     first_name = db.Column(
@@ -56,4 +61,51 @@ class Relationship(db.Model):
     relation_type = db.Column(
         db.String(20),
         nullable=False
+    )
+
+class Family(db.Model):
+    __tablename__ = "families"
+
+    id = db.Column(
+        db.String(36),
+        primary_key=True
+    )
+
+    # Tên dòng họ
+    name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    # Năm bắt đầu / lập họ / đời thủy tổ
+    founded_year = db.Column(
+        db.Integer
+    )
+
+    # Số chi nhánh
+    branch_number = db.Column(
+        db.Integer
+    )
+
+    ancestral_house_address = db.Column(
+        db.Text
+    )
+
+    # Dùng cho Google Map
+    latitude = db.Column(
+        db.Numeric(10, 8)
+    )
+
+    longitude = db.Column(
+        db.Numeric(11, 8)
+    )
+
+    # Mô tả thêm
+    description = db.Column(
+        db.Text
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now()
     )
