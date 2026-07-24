898acc6db24d9fa2f470e3c4d2d656f6c5ac1c83
huutuan238 <huutuan238@gmail.com>
2026-07-25 00:43:00 +0900

    update config db

--- backend/config.py
+++ backend/config.py
@@ -2,12 +2,13 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('DB_USER')}:"
-        f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:"
-        f"{os.getenv('DB_PORT')}/"
-        f"{os.getenv('DB_NAME')}"
-    )
+    # SQLALCHEMY_DATABASE_URI = (
+    #     f"postgresql://{os.getenv('DB_USER')}:"
+    #     f"{os.getenv('DB_PASSWORD')}@"
+    #     f"{os.getenv('DB_HOST')}:"
+    #     f"{os.getenv('DB_PORT')}/"
+    #     f"{os.getenv('DB_NAME')}"
+    # )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
