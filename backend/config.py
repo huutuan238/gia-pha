82d81115d46ee1783af67592e439fe5823075989
huutuan238 <huutuan238@gmail.com>
2026-08-03 01:03:37 +0900

    hotfix: get s3 env

--- backend/config.py
+++ backend/config.py
@@ -12,3 +12,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    AWS_REGION = os.getenv("AWS_REGION", "ap-southeast-1")
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
